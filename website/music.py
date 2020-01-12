ef54f24648f59d935bfb1eb38fcd6a90dcb914cd
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-01-12 19:35:18 +0200

    added a route (/music/get_song_audio_file) to download audio files

--- website/music.py
+++ website/music.py
@@ -1,9 +1,12 @@
 import functools
+from flask import current_app as app
+from flask import send_from_directory
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Blueprint
 )
 from website.db import get_music_db
 
+import os
 import sqlite3
 import json
 
@@ -13,6 +16,11 @@ def get_songs():
     db = get_music_db()
     db_songs = db.execute('SELECT * FROM songs').fetchall()
     return db_songs
+
+def get_song(id):
+    db = get_music_db()
+    db_song = db.execute('SELECT * FROM songs WHERE id = ?', (id,)).fetchone()
+    return db_song
 
 @bp.route('/all_songs', methods=['GET'])
 def all_songs():
@@ -27,3 +35,9 @@ def all_songs():
         songs.append(song)
 
     return json.dumps(songs)
+
+@bp.route('/get_song_audio_file/<int:song_id>', methods=['GET'])
+def song_audio_file(song_id):
+    song = get_song(song_id)
+    return send_from_directory(os.path.join(app.instance_path, 'audio'),
+                               os.path.basename(song['file_path']))
