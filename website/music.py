8813d2453673eeaa5749896d5c7baed5a1187611
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-04-24 20:47:22 +0300

    added music route, refactored, still working on music route, alot of work to do

--- website/music.py
+++ website/music.py
@@ -6,11 +6,24 @@ from flask import (
 from website import db
 from website.utils import current_time
 
-from pmus.db import MusicProvider
+from commandify.db import DBProvider
+cmdify_db_provider = DBProvider()
 
 bp = Blueprint('music', __name__, url_prefix='/music')
 
 @bp.route('/', methods=('GET',))
 def music_index():
-    songs = []
-    return render_template('music.html')
+    albums, tracks, artists = cmdify_db_provider.get_music()
+
+    for track in tracks:
+        ms_listened = track.ms_listened()
+        track.secs_listened = (ms_listened // 1000) % 60
+        track.mins_listened = (ms_listened // 60000) % 60
+        track.hrs_listened = (ms_listened // 360000)
+
+        track.trend = []
+        first_listen_time = track.plays[0].time_started
+        last_listen_time = track.plays[-1].time_ended
+        step_size = last_listen_time - first_listen_time
+    
+    return render_template('music.html', songs=tracks)
